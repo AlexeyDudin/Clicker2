640aac2146d270ae72f5ece137e246a7c2485dcd
Dudin Alexey <dudin.lesha@gmail.com>
2022-05-23 07:49:07 +0300

    Добавьте файлы проекта.

---
+++ Clicker2/src/Forms/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Clicker.src.Model;
+using MahApps.Metro.Controls;
+
+namespace Clicker2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow, INotifyPropertyChanged
+    {
+        private SeleniumParams selectedParam = new SeleniumParams();
+        private ObservableCollection<SeleniumParams> paramList = new ObservableCollection<SeleniumParams>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal ObservableCollection<SeleniumParams> ParamList
+        { 
+            get => paramList;
+            set
+            {
+                paramList = value;
+                OnPropertyChanged("ParamList");
+            }
+        }
+        internal SeleniumParams SelectedParam 
+        {
+            get => selectedParam;
+            set
+            {
+                selectedParam = value;
+                OnPropertyChanged("SelectedParam");
+            }
+        }
+
+        private void OnPropertyChanged(string name) 
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ Clicker2/src/Model/SeleniumParams.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clicker.src.Model
+{
+    public class SeleniumParams : INotifyPropertyChanged
+    {
+        private string paramName;
+        
+        private string findUrl;
+        private string request;
+
+        private Browsers browser;
+
+        private string finderUrl;
+
+        private List<string> explicitDomainList;
+
+        private bool gotoPageAndRunNext;
+        private bool gotoPageAndWait;
+        private bool gotoPageAndRun;
+        private int timeWork;
+
+        private IPAddress proxyIP;
+        private IPEndPoint proxyPort;
+        private string proxyLogin;
+        private string proxyPassword;
+
+        private string userAgent;
+
+        private bool useJS;
+        private bool useCookie;
+
+        private bool useTextLog;
+        private bool useImageLog;
+        private bool useVideoLog;
+
+        public Browsers Browser { get => browser; set => browser = value; }
+        public string FinderUrl { get => finderUrl; set => finderUrl = value; }
+        public IPAddress ProxyIP { get => proxyIP; set => proxyIP = value; }
+        public IPEndPoint ProxyPort { get => proxyPort; set => proxyPort = value; }
+        public string FindUrl { get => findUrl; set => findUrl = value; }
+        public string Request { get => request; set => request = value; }
+        public string ParamName { get => paramName; set => paramName = value; }
+        public string ProxyLogin { get => proxyLogin; set => proxyLogin = value; }
+        public string ProxyPassword { get => proxyPassword; set => proxyPassword = value; }
+        public List<string> ExplicitDomain { get => explicitDomainList; set => explicitDomainList = value; }
+        public bool GotoPageAndWait { get => gotoPageAndWait; set => gotoPageAndWait = value; }
+        public bool GotoPageAndRun { get => gotoPageAndRun; set => gotoPageAndRun = value; }
+        public int TimeWork { get => timeWork; set => timeWork = value; }
+        public bool UseJS { get => useJS; set => useJS = value; }
+        public bool UseCookie { get => useCookie; set => useCookie = value; }
+        public string UserAgent { get => userAgent; set => userAgent = value; }
+        public bool UseTextLog { get => useTextLog; set => useTextLog = value; }
+        public bool UseImageLog { get => useImageLog; set => useImageLog = value; }
+        public bool UseVideoLog { get => useVideoLog; set => useVideoLog = value; }
+        public bool GotoPageAndRunNext { get => gotoPageAndRunNext; set => gotoPageAndRunNext = value; }
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+    }
+}
